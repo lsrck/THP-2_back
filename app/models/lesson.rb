e6a80308231b2739dd7c882a589a9e3400b128a0
Louis Aurick <louisaurick@ntymail.com>
2018-07-05 16:19:12 +0200

    lesson model + constraints

---
+++ app/models/lesson.rb
@@ -0,0 +1,13 @@
+# == Schema Information
+#
+# Table name: lessons
+#
+#  id          :uuid             not null, primary key
+#  description :text
+#  title       :string(50)       not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+class Lesson < ApplicationRecord
+end
---
+++ spec/models/lesson_spec.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: lessons
+#
+#  id          :uuid             not null, primary key
+#  description :text
+#  title       :string(50)       not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+require 'rails_helper'
+
+RSpec.describe Lesson, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
