0dcff8ecc91d254323b5fcdeb4baf56b3f9a4de4
Louis Aurick <louisaurick@ntymail.com>
2018-07-09 18:42:15 +0200

    index test 200

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,18 +1,17 @@
 class LessonsController < ApplicationController
   def create
+    lessons = Lessons.order(created_at)
+    render json: lessons
   end
 
-  def index
-  end
+  def index; end
 
   def show
     lesson = Lesson.find(params[:id])
     render json: lesson
   end
 
-  def update
-  end
+  def update; end
 
-  def destroy
-  end
+  def destroy; end
 end
--- spec/controllers/lessons_controller_spec.rb
+++ spec/controllers/lessons_controller_spec.rb
@@ -2,13 +2,26 @@ require 'rails_helper'
 
 RSpec.describe LessonsController, type: :controller do
   describe "#create"
+
   describe "#index"
+    let(:lessons) { create_list(:lesson, 5) }
+
+    subject do
+      get :index
+    end
+
+    it "has a 200 status code" do
+      subject do
+        expect(json_response[:lessons].status).to eq(200)
+      end
+    end
+
   describe "#show" do
     let(:lesson) { create(:lesson) }
-    let(:id) { turtle.id }
-    
+    let(:id) { lesson.id }
+
     subject do
-      get :show, {params: { id: id } }
+      get :show, params: { id: id }
     end
 
     it "returns the turtle" do
@@ -16,10 +29,8 @@ RSpec.describe LessonsController, type: :controller do
         expect(json_response[:lesson][:id]).to eq(lesson.id)
         expect(json_response[:lesson][:title]).to eq(lesson.title)
         expect(json_response[:lesson][:description]).to eq(lesson.description)
-
       end
     end
-
   end
   describe "#update"
   describe "#destroy"
